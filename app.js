6dfd5d113a8e751d587713efa3eb2cbcad8a627d
yosia <yosialuther2803@gmail.com>
2024-07-11 16:55:43 +0700

    changing port

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ if (process.env.NODE_ENV !== "production") {
   const cors = require("cors");
   const app = express();
   const routes = require("./routes/index");
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   
   app.use(cors());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(routes);
+
 
   app.listen(port, () => {
     console.log(`App listening on port ${port}`);
